125c8d1f6ae379bdb87e6142bc659f9ef7ae6500
Diego Ramos <ramosdiego@pm.me>
2022-05-31 16:46:19 -0400

    add primitives

---
+++ components/primitives/Grid.tsx
@@ -0,0 +1,56 @@
+import { styled } from 'stitches.config'
+
+export const Grid = styled('div', {
+  display: 'grid',
+
+  variants: {
+    align: {
+      start: {
+        alignItems: 'start',
+      },
+      center: {
+        alignItems: 'center',
+      },
+      end: {
+        alignItems: 'end',
+      },
+      stretch: {
+        alignItems: 'stretch',
+      },
+      baseline: {
+        alignItems: 'baseline',
+      },
+    },
+    justify: {
+      start: {
+        justifyContent: 'start',
+      },
+      center: {
+        justifyContent: 'center',
+      },
+      end: {
+        justifyContent: 'end',
+      },
+      between: {
+        justifyContent: 'space-between',
+      },
+    },
+    flow: {
+      row: {
+        gridAutoFlow: 'row',
+      },
+      column: {
+        gridAutoFlow: 'column',
+      },
+      dense: {
+        gridAutoFlow: 'dense',
+      },
+      rowDense: {
+        gridAutoFlow: 'row dense',
+      },
+      columnDense: {
+        gridAutoFlow: 'column dense',
+      },
+    },
+  },
+})
