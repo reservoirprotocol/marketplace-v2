ec09c2012275fbb8cf2f4a495eecc6b36d718550
Diego Ramos <ramosdiego@pm.me>
2022-06-01 15:10:34 -0400

    Added text component

---
+++ components/primitives/Text.tsx
@@ -0,0 +1,53 @@
+import { styled } from 'stitches.config'
+
+export const Text = styled('span', {
+  color: '$gray12',
+  fontFamily: 'Inter',
+  letterSpacing: 0,
+
+  variants: {
+    style: {
+      h2: {
+        fontWeight: 700,
+        fontSize: 48,
+      },
+      h4: {
+        fontWeight: 700,
+        fontSize: 24,
+      },
+      h5: {
+        fontWeight: 700,
+        fontSize: 20,
+      },
+      h6: {
+        fontWeight: 700,
+        fontSize: 16,
+      },
+      subtitle1: {
+        fontWeight: 500,
+        fontSize: 16,
+      },
+      subtitle2: {
+        fontWeight: 500,
+        fontSize: 12,
+      },
+      body1: {
+        fontWeight: 400,
+        fontSize: 16,
+      },
+      body2: {
+        fontWeight: 400,
+        fontSize: 12,
+      },
+    },
+    italic: {
+      true: {
+        fontStyle: 'italic',
+      },
+    },
+  },
+
+  defaultVariants: {
+    style: 'body1',
+  },
+})
