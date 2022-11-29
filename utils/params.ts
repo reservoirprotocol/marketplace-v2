6e05e4146c03c8000067395718d89141a9ec0afb
pedromcunha <pedro.spinsomewebs@gmail.com>
2022-11-29 13:57:33 -0500

    Remove unnecessary files

--- utils/params.ts
+++
@@ -1,9 +0,0 @@
-export const paramsToQueryString = (params: {}) => {
-  if (!params) {
-    return ''
-  }
-
-  return Object.keys(params)
-    .map((key) => `${key}=${params[key]}`)
-    .join('&')
-}
