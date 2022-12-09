59755368b99d452e0b3a9ad9283e80c6ea927729
Ted Palmer <etpalmer17@gmail.com>
2022-12-09 13:54:35 -0500

    fixed typescript error in utils/fetcher

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -5,10 +5,10 @@ const fetcher = async (
   params: Record<string, any> = {},
   data: RequestInit = {}
 ) => {
-  const headers: RequestInit['headers'] = data['headers'] || {}
+  const headers = new Headers()
 
   if (process.env.NEXT_PUBLIC_RESERVOIR_API_KEY) {
-    headers['x-api-key'] = process.env.NEXT_PUBLIC_RESERVOIR_API_KEY
+    headers.set('x-api-key', process.env.NEXT_PUBLIC_RESERVOIR_API_KEY)
   }
 
   const path = new URL(`${process.env.NEXT_PUBLIC_RESERVOIR_API_BASE}/${url}`)
