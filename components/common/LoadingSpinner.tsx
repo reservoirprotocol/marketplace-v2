e3ad8cad76fb95d9b766f13fbda9c78c46f6a35d
Ted Palmer <etpalmer17@gmail.com>
2023-01-03 12:29:06 -0500

    added loading and empty states to collections page and fixed empty attributes

---
+++ components/common/LoadingSpinner.tsx
@@ -0,0 +1,26 @@
+import { keyframes } from '@stitches/react'
+import { Box } from 'components/primitives'
+
+const LoadingSpinner = () => {
+  const spin = keyframes({
+    '0%': { transform: 'rotate(0deg)' },
+    '100%': { transform: 'rotate(360deg)' },
+  })
+
+  return (
+    <Box
+      css={{
+        width: 40,
+        height: 40,
+        border: '5px solid transparent',
+        borderBottomColor: '$primary9',
+        borderRightColor: 'transparent',
+        borderRadius: '50%',
+        display: 'inline-block',
+        animation: `${spin} 1s cubic-bezier(0.76, 0.35, 0.2, 0.7) infinite`,
+      }}
+    ></Box>
+  )
+}
+
+export default LoadingSpinner
