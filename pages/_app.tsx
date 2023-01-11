f6f853c4ab4b86839835d876230086eb461fd8a8
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-01-11 16:22:26 -0500

    Strip url trailing slash from vercel url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -156,8 +156,10 @@ AppWrapper.getInitialProps = async (appContext: AppContext) => {
   if (appContext.ctx.req?.headers.host) {
     baseUrl = `http://${appContext.ctx.req?.headers.host}`
   } else if (process.env.VERCEL_URL) {
-    baseUrl = process.env.VERCEL_URL
+    baseUrl = `${process.env.VERCEL_URL}`
   }
+
+  baseUrl = baseUrl.replace(/\/$/, '')
 
   return { ...appProps, baseUrl }
 }
