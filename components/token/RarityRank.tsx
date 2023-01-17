56a7a830286484e62284666c7bd84edefc67fdc2
Ted Palmer <etpalmer17@gmail.com>
2023-01-16 20:27:40 -0500

    Added 'Top x%' logic to rarity rank

--- components/token/RarityRank.tsx
+++ components/token/RarityRank.tsx
@@ -13,15 +13,41 @@ type Props = {
 }
 
 export default ({ token, collection, collectionAttributes }: Props) => {
+  const rarityRank = token?.token?.rarityRank
+  const tokenCount = collection?.tokenCount
+  const rankPercentile = Math.floor(
+    ((rarityRank as number) / parseInt(tokenCount as string)) * 100
+  )
+
+  let displayPercentile = rankPercentile
+
+  if (displayPercentile <= 1) {
+    displayPercentile = 1
+  } else if (displayPercentile <= 5) {
+    displayPercentile = 5
+  } else if (displayPercentile <= 10) {
+    displayPercentile = 10
+  } else if (displayPercentile <= 15) {
+    displayPercentile = 15
+  } else if (displayPercentile <= 20) {
+    displayPercentile = 20
+  } else if (displayPercentile <= 30) {
+    displayPercentile = 30
+  } else if (displayPercentile <= 40) {
+    displayPercentile = 40
+  } else if (displayPercentile <= 50) {
+    displayPercentile = 50
+  }
+
   const rarityEnabledCollection =
-    collection?.tokenCount &&
-    +collection.tokenCount >= 2 &&
+    tokenCount &&
+    +tokenCount >= 2 &&
     collectionAttributes &&
     collectionAttributes?.length >= 2
 
   if (
-    token?.token?.rarityRank === undefined ||
-    token?.token?.rarityRank === null ||
+    rarityRank === undefined ||
+    rarityRank === null ||
     token?.token?.kind === 'erc1155' ||
     !rarityEnabledCollection
   ) {
@@ -32,9 +58,13 @@ export default ({ token, collection, collectionAttributes }: Props) => {
     <Tooltip
       content={
         <Flex direction="column" align="start">
+          {displayPercentile <= 50 && (
+            <Text style="body2">Top {displayPercentile}%</Text>
+          )}
           <Text style="body2" ellipsify>
-            {`${formatNumber(token?.token?.rarityRank)}/
-            ${formatNumber(collection.tokenCount)}`}
+            Rarity rank:{' '}
+            {`${formatNumber(rarityRank)}/
+            ${formatNumber(tokenCount)}`}
           </Text>
           <Text style="body2" color="subtle">
             By Poprank
@@ -59,7 +89,7 @@ export default ({ token, collection, collectionAttributes }: Props) => {
         <Text style="subtitle2" css={{ color: '$gray11' }}>
           Rank
         </Text>
-        <Text style="subtitle2">{formatNumber(token?.token?.rarityRank)} </Text>
+        <Text style="subtitle2">{formatNumber(rarityRank)} </Text>
       </Flex>
     </Tooltip>
   )
