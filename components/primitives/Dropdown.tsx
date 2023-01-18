45bd13038bbe02ba5b80464aa2957919eedfecf3
Ted Palmer <etpalmer17@gmail.com>
2023-01-18 15:44:35 -0500

    Sped up dropdown animation

--- components/primitives/Dropdown.tsx
+++ components/primitives/Dropdown.tsx
@@ -31,13 +31,13 @@ const AnimatedDropdownMenuContent = forwardRef<
         scale: 1,
         opacity: 1,
         y: 0,
-        transition: { mass: 1, type: 'spring', duration: 0.15 },
+        transition: { mass: 0.05, type: 'spring', duration: 0.1 },
       }}
       exit={{
         y: -20,
         scale: 0.9,
         opacity: 0,
-        transition: { duration: 0.3 },
+        transition: { duration: 0.1 },
       }}
     >
       {children}
