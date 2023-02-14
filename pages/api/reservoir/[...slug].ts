c15ce5712f80cc4749d0ca669138dc205405276b
Jamie Chicago <87397251+jamiechicago312@users.noreply.github.com>
2023-02-14 11:54:01 -0600

    Update [...slug].ts
    revert

--- pages/api/reservoir/[...slug].ts
+++ pages/api/reservoir/[...slug].ts
@@ -65,9 +65,7 @@ const proxy = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const headers = new Headers()
 
-    //'x-api-key: 0x21d426e2B88Fbf8b93adf0591A2D5B08d58F089f'
-
-    if (chain.0x21d426e2B88Fbf8b93adf0591A2D5B08d58F089f) headers.set('x-api-key', chain.0x21d426e2B88Fbf8b93adf0591A2D5B08d58F089f)
+    if (chain.apiKey) headers.set('x-api-key', chain.apiKey)
 
     if (typeof body === 'object') {
       headers.set('Content-Type', 'application/json')
