cbe4c91d657927c9aa174650f14603d296553001
Ted Palmer <etpalmer17@gmail.com>
2023-02-17 12:20:43 -0500

    Fixes issues with toasts

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,12 @@
-import { Box, Toast } from 'components/primitives'
-import Head from 'next/head'
-import { FC, ReactNode, useContext } from 'react'
+import { Box } from 'components/primitives'
+import { FC, ReactNode } from 'react'
 import Navbar from './navbar'
-import { ToastContext } from '../context/ToastContextProvider'
 
 type Props = {
   children: ReactNode
 }
 
 const Layout: FC<Props> = ({ children }) => {
-  const { toasts } = useContext(ToastContext)
-
   return (
     <>
       <Box
@@ -23,18 +19,7 @@ const Layout: FC<Props> = ({ children }) => {
       >
         <Box css={{ maxWidth: 1920, mx: 'auto' }}>
           <Navbar />
-
           <main>{children}</main>
-          {toasts.map((toast, idx) => {
-            return (
-              <Toast
-                key={idx}
-                title={toast.title}
-                description={toast.description}
-                action={toast.action}
-              />
-            )
-          })}
         </Box>
       </Box>
     </>
