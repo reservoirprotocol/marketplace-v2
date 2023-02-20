2aaa63b46ea7445319c6e73d92b5e11951c09562
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-02-21 01:56:55 +0700

    + Only Eligible Wallet And listed

--- pages/api/claim.ts
+++ pages/api/claim.ts
@@ -1,18 +1,17 @@
-import fs from 'fs';
 import type {NextApiRequest, NextApiResponse} from "next";
 import {paths} from "@nftearth/reservoir-sdk";
 import fetcher from "utils/fetcher";
 import supportedChains from "utils/chains";
 
 // use required instead import because of deprecation
-const Web3 = require('web3');
+// const Web3 = require('web3');
 
 // First Eligible 1786 Address
 import rawSignatures from '../../data/signatures.json';
-const web3 = new Web3(`https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`);
-const signer = web3.eth.accounts.privateKeyToAccount(
-  process.env.SIGNER_PKEY as string
-);
+// const web3 = new Web3(`https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`);
+// const signer = web3.eth.accounts.privateKeyToAccount(
+//   process.env.SIGNER_PKEY as string
+// );
 
 const signatures: {[key: string]: string} = rawSignatures;
 
@@ -21,12 +20,6 @@ export default async function claim(req: NextApiRequest, res: NextApiResponse) {
   if (!address) {
     return res.send({
       result: null
-    });
-  }
-
-  if (signatures[address.toLowerCase()]) {
-    return res.send({
-      result: signatures[address.toLowerCase()]
     });
   }
 
@@ -58,11 +51,11 @@ export default async function claim(req: NextApiRequest, res: NextApiResponse) {
     })
 
     if (results.length > 0) {
-      let message = `0x000000000000000000000000${address.substring(2)}`;
-      let {signature} = signer.sign(message);
+      // let message = `0x000000000000000000000000${address.substring(2)}`;
+      // let {signature} = signer.sign(message);
 
       return res.send({
-        result: signature
+        result: signatures[address.toLowerCase()] || null
       });
     }
 
