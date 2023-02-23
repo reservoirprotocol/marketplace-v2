407dfe41a7d41bcd97d626595c9055aba3336543
shawnkeqin <e0052482@u.nus.edu>
2023-02-23 21:42:54 +0800

    feat: add launchpad tile enums info

---
+++ components/launch/constants/index.ts
@@ -0,0 +1,33 @@
+export const LaunchPadTileInfo = [
+  {
+    title: 'Art generation',
+    description: '',
+    link: '',
+  },
+  {
+    title: 'Smart contracts',
+    description: '',
+    link: '',
+  },
+  {
+    title: 'Allowlist management',
+    description: '',
+    link: '',
+  },
+  {
+    title: 'Embed button for use in apps',
+    description: '',
+    link: '',
+  },
+  {
+    title: 'Presale/Public sale custimizations',
+    description: '',
+    link: '',
+  },
+  {
+    title: 'Creator Privilege',
+    description:
+      'Ability for creator to have their own portal to set all things they want: e.g. Twitter, Discord, Royalties, GitHub, their own Affiliate links, etc.',
+    link: '',
+  },
+]
