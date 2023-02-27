8fc3b2c3aa53599d5618f7b8a0dc6d3419b6521e
Dark Night <darknight.dev0918@gmail.com>
2023-02-27 23:01:37 +0900

    Finished Structre Quest Page

--- components/quests/QuestsSection.tsx
+++ components/quests/QuestsSection.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, Text } from '../primitives'
+import { Flex, Box, Text } from '../primitives'
 import { FC } from 'react'
 
 const QuestsSection: FC = () => {
@@ -9,39 +9,61 @@ const QuestsSection: FC = () => {
       css={{
         width: '100%',
         backgroundPosition: 'center center',
+        flexWrap: 'wrap',
         backgroundImage: `linear-gradient(109.6deg, rgb(0, 0, 0) 11.2%, $primary13 91.1%), url('/images/heroSectionBanner.png')`,
+        borderRadius: '16px',
         '@xs': {
-          gridTemplateColumns: 'unset',
-          padding: '64px 24px',
+          padding: '0 50px',
         },
         '@lg': {
-          gridTemplateColumns: 'repeat(2, 1fr)',
-          padding: '100px 64px',
+          padding: '0 70px',
         },
       }}
     >
-      <Grid
-        css={{
-          gap: 32,
-          '@xs': {
-            flex: 1,
-          },
-          '@lg': {
-            flex: 0.5,
-          },
-        }}
-      >
+      <Box css={{
+        marginTop: '-50px',
+        '@xs': {
+          margin: '0 auto',
+        },
+        '@bp1000': {
+          margin: 0,
+        },
+      }}>
+        <img src="/images/Quests_1.png" />
+      </Box>
         <Text
           style={{
-            '@initial': 'h2',
+            '@initial': 'h3',
+            '@md': 'h2',
             '@lg': 'h1',
           }}
           as="h1"
-          css={{ color: '$whiteA12', lineHeight: 1.2 }}
+          css={{
+            color: '$whiteA12',
+            lineHeight: 1.2,
+            '@xs': {
+              textAlign: 'center',
+              marginBottom: '$5',
+            },
+            '@bp1000': {
+              margin: 'auto',
+              textAlign: 'left',
+            },
+          }}
         >
           Let the Quest begin!
-        </Text>
-      </Grid>
+      </Text>
+      <Box css={{
+        marginTop: '-50px',
+        '@xs': {
+          display: 'none',
+        },
+        '@bp1400': {
+          display: 'block'
+        },
+      }}>
+        <img src="/images/Quests_2.png" />
+      </Box>
     </Flex>
   )
 }
