537197dca6d22925ccf295f5e39a61682bc26d55
shawnkeqin <e0052482@u.nus.edu>
2023-02-27 13:22:09 +0800

    feature: leaderboard table WIP

---
+++ components/leaderboard/enums/index.ts
@@ -0,0 +1,62 @@
+export const defaultData = [
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+  {
+    rank: 'tanner',
+    codeName: 'linsley',
+    volume: 24,
+    potentialReward: 100,
+  },
+]
