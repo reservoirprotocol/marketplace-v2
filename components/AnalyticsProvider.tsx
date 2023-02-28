962b12550ab7d402adac1d7ccad23b43e8744331
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-02-28 14:57:27 -0500

    Initialize analytics provider before any css

--- components/AnalyticsProvider.tsx
+++ components/AnalyticsProvider.tsx
@@ -10,6 +10,30 @@ type Props = {
   children: ReactElement
 }
 
+export const initializeAnalytics = () => {
+  debugger
+  if (typeof window !== 'undefined' && !datadogRum.getInitConfiguration()) {
+    if (!ddApplicationId || !ddClientToken) return
+
+    datadogRum.init({
+      applicationId: ddApplicationId,
+      clientToken: ddClientToken,
+      site: 'datadoghq.com',
+      //CONFIGURABLE: Change the service name to customize how it appears in your DD dashboard
+      service: 'reservoir-marketplace',
+      env,
+      sampleRate: 100,
+      replaySampleRate: 100,
+      trackInteractions: true,
+      trackFrustrations: true,
+      trackResources: true,
+      defaultPrivacyLevel: 'mask-user-input',
+    })
+
+    datadogRum.startSessionReplayRecording()
+  }
+}
+
 const AnalyticsProvider: FC<Props> = ({ children }) => {
   const accountData = useAccount()
 
@@ -20,28 +44,6 @@ const AnalyticsProvider: FC<Props> = ({ children }) => {
       })
     }
   }, [accountData])
-
-  useEffect(() => {
-    if (typeof window !== 'undefined' && !datadogRum.getInitConfiguration()) {
-      if (!ddApplicationId || !ddClientToken) return
-
-      datadogRum.init({
-        applicationId: ddApplicationId,
-        clientToken: ddClientToken,
-        site: 'datadoghq.com',
-        service: 'reservoir-marketplace',
-        env,
-        sampleRate: 100,
-        replaySampleRate: 100,
-        trackInteractions: true,
-        trackFrustrations: true,
-        trackResources: true,
-        defaultPrivacyLevel: 'mask-user-input',
-      })
-
-      datadogRum.startSessionReplayRecording()
-    }
-  }, [])
 
   return children
 }
