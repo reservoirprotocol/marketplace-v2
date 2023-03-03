dc4091d379376ee79447f6fcc906842a76b1474a
shawnkeqin <e0052482@u.nus.edu>
2023-03-03 10:18:15 +0800

    chore: add quest title and description

--- components/quests/QuestsGrid.tsx
+++ components/quests/QuestsGrid.tsx
@@ -74,10 +74,10 @@ export const QuestsGrid = ({ setOpen, setQuest }: Props) => {
       </Box>
       <Box>
         <Quest
-          title="Register a username"
-          description="Assign a username to your wallet from the Profile Settings page."
+          title="List your NFT in NFTE currency on ANY chain"
+          description="List at least ANY 1 NFT in NFTE currency at ANY price on ANY chain"
           points={25}
-          locked={true}
+          locked={false}
           link=""
           setOpen={setOpen}
           setQuest={setQuest}
