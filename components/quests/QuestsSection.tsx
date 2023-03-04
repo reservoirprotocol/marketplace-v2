2baea70001ab6c3464112e248b59f4800a222d38
shawnkeqin <e0052482@u.nus.edu>
2023-03-04 14:15:42 +0800

    chore: update header and subheader for quests

--- components/quests/QuestsSection.tsx
+++ components/quests/QuestsSection.tsx
@@ -2,7 +2,6 @@ import { Flex, Box, Text } from '../primitives'
 import { FC } from 'react'
 
 const QuestsSection: FC = () => {
-
   return (
     <Flex
       as="section"
@@ -20,50 +19,72 @@ const QuestsSection: FC = () => {
         },
       }}
     >
-      <Box css={{
-        marginTop: '-50px',
-        '@xs': {
-          margin: '0 auto',
-        },
-        '@bp1000': {
-          margin: 0,
-        },
-      }}>
+      <Box
+        css={{
+          marginTop: '-50px',
+          '@xs': {
+            margin: '0 auto',
+          },
+          '@bp1000': {
+            margin: 0,
+          },
+        }}
+      >
         <img src="/images/Quests_1.png" />
       </Box>
+
+      <Flex
+        direction="column"
+        css={{ marginLeft: 'auto', marginRight: 'auto', alignItems: 'center' }}
+      >
         <Text
           css={{
             color: '$whiteA12',
             lineHeight: 1.2,
             '@xs': {
               textAlign: 'center',
-              margin: 'auto',
-              marginBottom: '$5',
+              marginTop: '100px',
+
               fontSize: '40px',
-              fontWeight: '700'
+              fontWeight: '700',
             },
             '@sm': {
-              fontSize: '50px',  
+              fontSize: '50px',
             },
             '@bp900': {
-              margin: 'auto',
-              textAlign: 'left',
               fontSize: '60px',
-              fontWeight: '700'
+              fontWeight: '700',
             },
           }}
         >
-          Let the Quest begin!
-      </Text>
-      <Box css={{
-        marginTop: '-50px',
-        '@xs': {
-          display: 'none',
-        },
-        '@bp1400': {
-          display: 'block'
-        },
-      }}>
+          NFTEarth Quests
+        </Text>
+        <Text
+          style="subtitle1"
+          css={{
+            lineHeight: 1.5,
+            color: '$whiteA12',
+            width: '100%',
+            textAlign: 'center',
+            marginTop: '$4',
+            '@lg': { width: '100%' },
+          }}
+        >
+          Complete quests and tasks, earn XP and rewards!
+        </Text>
+      </Flex>
+
+      <Box
+        css={{
+          marginTop: '-50px',
+          '@xs': {
+            display: 'none',
+          },
+          '@bp1400': {
+            display: 'block',
+          },
+        }}
+      >
         <img src="/images/Quests_2.png" />
       </Box>
     </Flex>
