10f5428fe3819e2e79c029275a5ffa427a42b28c
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-03-08 21:44:41 +0700

    + Debug

--- pages/api/orderbook/listings.ts
+++ pages/api/orderbook/listings.ts
@@ -50,7 +50,7 @@ const handleOrderbookListings = async (req: NextApiRequest, res: NextApiResponse
   const collections: paths["/collections/v5"]["get"]["responses"]["200"]["schema"]["collections"] = data?.collections || []
   const collection = collections?.[0]
 
-  process.stdout.write(`New offer processed for ${parameters.offerer} data: ${JSON.stringify(parameters)}`)
+  console.info(`New listing processed for ${parameters.offerer}`, parameters)
 
   if (accountData && collection) {
     // TODO: Calculate reward by floor price & increase reward by listing period & double Reward for NFTE Token
