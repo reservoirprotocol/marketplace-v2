038d2bcb5b47bc4d8ed6c78febecc354f1078930
armando <armandomvillanueva@icloud.com>
2023-03-07 19:31:13 -0800

    Update till func to handle minute cases

--- utils/till.ts
+++ utils/till.ts
@@ -1,17 +1,26 @@
-import dayjs, { UnitType } from 'dayjs'
+import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.extend(relativeTime)
 
+type TimeTill = {
+  time: number
+  format: 'hour' | 'minute'
+}
+
 /**
- * Returns the time remaining from a given timestamp
- * @param timeStamp - A string in the format of "HH:MM:SS" representing the target time
- * @param format - (Optional) A string representing the unit of time to return the difference in (e.g., 'hour', 'minute', 'second', etc.). Defaults to 'hour'.
- * @returns The time remaining between the current time and the target time, in the specified format.
+ * Calculate the time remaining until a specified time
+ * @param {string} timeString - The time to calculate the remaining time until, in the format 'HH:mm:ss'
+ * @returns {{time: number, format: Format}} The remaining time until the specified time, in the format {time, format}
  */
-export function timeTill(timeStamp: string, format: UnitType = 'hour') {
-    const date = dayjs().set('hour', parseInt(timeStamp.slice(0, 2)));
-    date.set('minute', parseInt(timeStamp.slice(3, 5)));
-    date.set('second', parseInt(timeStamp.slice(6, 8)));
-    return date.diff(dayjs(), format);
-};
+export function timeTill(timeString: string): TimeTill {
+  const date = dayjs(timeString, 'HH:mm:ss').diff(dayjs(), 'minute')
+
+  const format = date >= 60 ? 'hour' : 'minute'
+  const time = format === 'hour' ? Math.floor(date / 60) : date
+
+  return {
+    time,
+    format,
+  }
+}
