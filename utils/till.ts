343f2269171955d5d15b977f16c45812dde9cfc4
armando <armandomvillanueva@icloud.com>
2023-03-09 09:50:30 -0800

    chore: Use relative time to simplify logic

--- utils/till.ts
+++ utils/till.ts
@@ -15,17 +15,5 @@ export const DATE_REGEX = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
  * @returns A human-readable string indicating the time remaining until the target time.
  */
 export const timeTill = (timeString: string | undefined): string => {
-  const duration = dayjs(timeString).diff(dayjs(), 'second')
-  const durationInMinutes = Math.floor(duration / 60)
-
-  const remainingHours = Math.floor(durationInMinutes / 60)
-  const remainingMinutes = durationInMinutes % 60
-
-  if (remainingHours > 0) {
-    return `${remainingHours} hour${
-      remainingHours > 1 ? 's' : ''
-    } and ${remainingMinutes} minute${remainingMinutes > 1 ? 's' : ''}.`
-  } else {
-    return `${remainingMinutes} minute${remainingMinutes > 1 ? 's' : ''}.`
-  }
+  return `Remaining time: ${dayjs(timeString).fromNow(true)}.`
 }
