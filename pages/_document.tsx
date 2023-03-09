ba2ee3081aa6137e0c4804a17d64b88b95001c1b
armando <armandomvillanueva@icloud.com>
2023-03-09 08:03:57 -0800

    chore: Formatting stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,9 +22,11 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
         </Head>
+        
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         {/* Meta tags */}
         <meta name="keywords" content="nft, ethereum, protocol" />
         <meta name="keywords" content="NFT, API, Protocol" />
