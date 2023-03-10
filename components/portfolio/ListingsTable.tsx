4509fd7eb4155488d4fba3d0b33ea4849946bf87
armando <armandomvillanueva@icloud.com>
2023-03-10 11:12:21 -0800

    feat: Add comments to zone Addresses

--- components/portfolio/ListingsTable.tsx
+++ components/portfolio/ListingsTable.tsx
@@ -37,8 +37,8 @@ type Props = {
 const desktopTemplateColumns = '1.25fr .75fr repeat(3, 1fr)'
 
 const zoneAddresses = [
-  '0xe1066481cc3b038badd0c68dfa5c8f163c3ff192', // Ethereum Address
-  '0x49b91d1d7b9896d28d370b75b92c2c78c1ac984a', // Goerli Address
+  '0xe1066481cc3b038badd0c68dfa5c8f163c3ff192', // Ethereum - 0xe1...92
+  '0x49b91d1d7b9896d28d370b75b92c2c78c1ac984a', // Goerli Address - 0x49...4a
 ]
 export const ListingsTable: FC<Props> = ({ address }) => {
   const loadMoreRef = useRef<HTMLDivElement>(null)
