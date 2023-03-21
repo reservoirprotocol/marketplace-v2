aa60baa99d24d075326586fc9b5ffca5a4024543
armando <armandomvillanueva@icloud.com>
2023-03-21 15:18:37 -0700

    feat: Add oracle cancel button to bids

--- components/portfolio/OffersTable.tsx
+++ components/portfolio/OffersTable.tsx
@@ -10,6 +10,7 @@ import {
   Anchor,
   Button,
   Box,
+  Tooltip,
 } from '../primitives'
 import Image from 'next/image'
 import { useIntersectionObserver } from 'usehooks-ts'
@@ -21,7 +22,7 @@ import { useMarketplaceChain, useTimeSince } from 'hooks'
 import CancelBid from 'components/buttons/CancelBid'
 import { Address } from 'wagmi'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHand } from '@fortawesome/free-solid-svg-icons'
+import { faGasPump, faHand } from '@fortawesome/free-solid-svg-icons'
 import { COMMUNITY } from 'pages/_app'
 import { NAVBAR_HEIGHT } from 'components/navbar'
 
@@ -30,6 +31,10 @@ type Props = {
 }
 
 const desktopTemplateColumns = '1.25fr .75fr repeat(3, 1fr)'
+const zoneAddresses = [
+  '0xe1066481cc3b038badd0c68dfa5c8f163c3ff192', // Ethereum - 0xe1...92
+  '0x49b91d1d7b9896d28d370b75b92c2c78c1ac984a', // Goerli Address - 0x49...4a
+]
 
 export const OffersTable: FC<Props> = ({ address }) => {
   const loadMoreRef = useRef<HTMLDivElement>(null)
@@ -38,6 +43,7 @@ export const OffersTable: FC<Props> = ({ address }) => {
   let bidsQuery: Parameters<typeof useBids>['0'] = {
     maker: address,
     includeCriteriaMetadata: true,
+    includeRawData: true,
   }
 
   if (COMMUNITY) bidsQuery.community = COMMUNITY
@@ -103,6 +109,12 @@ const OfferTableRow: FC<OfferTableRowProps> = ({ offer, mutate }) => {
   const isSmallDevice = useMediaQuery({ maxWidth: 900 })
   const { routePrefix } = useMarketplaceChain()
   const expiration = useTimeSince(offer?.expiration)
+
+  const orderZone = offer?.rawData?.zone
+  const orderKind = offer?.kind
+
+  const isOracleOrder =
+    orderKind === 'seaport-v1.4' && zoneAddresses.includes(orderZone as string)
 
   let criteriaData = offer?.criteria?.data
 
@@ -186,9 +198,31 @@ const OfferTableRow: FC<OfferTableRowProps> = ({ offer, mutate }) => {
             bidId={offer?.id as string}
             mutate={mutate}
             trigger={
-              <Button css={{ color: '$red11' }} color="gray3">
-                Cancel
-              </Button>
+              <Flex>
+                {!isOracleOrder ? (
+                  <Tooltip
+                    content={
+                      <Text style="body2" as="p">
+                        Cancelling this order requires gas.
+                      </Text>
+                    }
+                  >
+                    <Button css={{ color: '$red11' }} color="gray3">
+                      <FontAwesomeIcon
+                        color="#697177"
+                        icon={faGasPump}
+                        width="16"
+                        height="16"
+                      />
+                      Cancel
+                    </Button>
+                  </Tooltip>
+                ) : (
+                  <Button css={{ color: '$red11' }} color="gray3">
+                    Cancel
+                  </Button>
+                )}
+              </Flex>
             }
           />
         </Flex>
@@ -272,9 +306,31 @@ const OfferTableRow: FC<OfferTableRowProps> = ({ offer, mutate }) => {
             bidId={offer?.id as string}
             mutate={mutate}
             trigger={
-              <Button css={{ color: '$red11' }} color="gray3">
-                Cancel
-              </Button>
+              <Flex>
+                {!isOracleOrder ? (
+                  <Tooltip
+                    content={
+                      <Text style="body2" as="p">
+                        Cancelling this order requires gas.
+                      </Text>
+                    }
+                  >
+                    <Button css={{ color: '$red11' }} color="gray3">
+                      <FontAwesomeIcon
+                        color="#697177"
+                        icon={faGasPump}
+                        width="16"
+                        height="16"
+                      />
+                      Cancel
+                    </Button>
+                  </Tooltip>
+                ) : (
+                  <Button css={{ color: '$red11' }} color="gray3">
+                    Cancel
+                  </Button>
+                )}
+              </Flex>
             }
           />
         </Flex>
