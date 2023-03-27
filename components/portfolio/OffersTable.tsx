424bfc671ce7fca09adbd313b561e76e762d6419
armando <armandomvillanueva@icloud.com>
2023-03-27 08:59:10 -0700

    fix: Update address comments

--- components/portfolio/OffersTable.tsx
+++ components/portfolio/OffersTable.tsx
@@ -32,7 +32,7 @@ type Props = {
 
 const desktopTemplateColumns = '1.25fr .75fr repeat(3, 1fr)'
 const zoneAddresses = [
-  '0xaa0e012d35cf7d6ecb6c2bf861e71248501d3226', // Ethereum - 0xe1...92
+  '0xaa0e012d35cf7d6ecb6c2bf861e71248501d3226', // Ethereum - 0xaa...26
   '0x49b91d1d7b9896d28d370b75b92c2c78c1ac984a', // Goerli Address - 0x49...4a
 ]
 
