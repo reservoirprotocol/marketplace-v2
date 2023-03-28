398830db971d5c97dffe6102450b71ebb51f1b8c
Ted Palmer <etpalmer17@gmail.com>
2023-03-28 12:23:30 -0400

    improve coingecko error handling, add 5 sec timeout

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -30,4 +30,27 @@ export const basicFetcher = async (href: string, options?: RequestInit) => {
   return { data: json, response }
 }
 
+export const fetchWithTimeout = async (
+  url: string,
+  options: RequestInit,
+  timeout: number
+): Promise<Response> => {
+  const controller = new AbortController()
+  const signal = controller.signal
+  const timeoutId = setTimeout(() => controller.abort(), timeout)
+
+  try {
+    const response = await fetch(url, { ...options, signal })
+    clearTimeout(timeoutId)
+    return response
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      if (error.name === 'AbortError') {
+        throw new Error('Request timed out')
+      }
+    }
+    throw error
+  }
+}
+
 export default fetcher
