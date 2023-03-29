9f491365e725a66752b0a2e7f3bb8ef3056ef992
TomCat <midoelmelegy@gmail.com>
2023-03-29 22:48:02 +0800

    Update ThemeSwitcher.tsx

--- components/navbar/ThemeSwitcher.tsx
+++ components/navbar/ThemeSwitcher.tsx
@@ -1,74 +1,24 @@
 import { useTheme } from 'next-themes'
-import { Box, Button, ToggleGroupRoot, ToggleGroupItem } from '../primitives'
+import { Box, Button } from '../primitives'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
-import { useMounted } from '../../hooks'
-import { useMediaQuery } from 'react-responsive'
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme()
-  const isMounted = useMounted()
-  const isMobile = useMediaQuery({ maxWidth: 600 }) && isMounted
-
-  if (isMobile) {
-    return (
-      <ToggleGroupRoot
-        type="single"
-        defaultValue={theme}
-        css={{
-          borderRadius: 0,
-        }}
-        aria-label="Change theme"
-      >
-        <ToggleGroupItem
-          onClick={() => setTheme('light')}
-          value="light"
-          disabled={theme == 'light'}
-          aria-label="Light Mode"
-          css={{
-            flex: 0.5,
-            p: '$1',
-          }}
-        >
-          <FontAwesomeIcon icon={faSun} width={16} height={16} />
-        </ToggleGroupItem>
-        <ToggleGroupItem
-          onClick={() => setTheme('dark')}
-          value="dark"
-          disabled={theme == 'dark'}
-          aria-label="Dark Mode"
-          css={{
-            flex: 0.5,
-            p: '$1',
-          }}
-        >
-          <FontAwesomeIcon icon={faMoon} width={16} height={16} />
-        </ToggleGroupItem>
-      </ToggleGroupRoot>
-    )
-  }
 
   return (
     <Box>
       <Button
-        css={{
-          justifyContent: 'center',
-          width: '44px',
-          height: '44px',
-          '&:hover': {
-            background: '$gray8',
-          },
-        }}
+        css={{ justifyContent: 'center', width: '44px', height: '44px' }}
         type="button"
-        onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}
+        onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
         size="small"
         color="gray3"
-        aria-label="Theme Switcher"
       >
         {theme == 'dark' ? (
-          <FontAwesomeIcon icon={faSun} width={16} height={16} />
-        ) : (
           <FontAwesomeIcon icon={faMoon} width={16} height={16} />
+        ) : (
+          <FontAwesomeIcon icon={faSun} width={16} height={16} />
         )}
       </Button>
     </Box>
