389c425a3d508fc22fc895d7660c1e8bf5ec25cd
Ted Palmer <etpalmer17@gmail.com>
2023-04-10 13:23:50 -0400

    Removed log level from ReservoirKit configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,6 @@ import ToastContextProvider from 'context/ToastContextProvider'
 import supportedChains from 'utils/chains'
 import { useMarketplaceChain } from 'hooks'
 import ChainContextProvider from 'context/ChainContextProvider'
-import { LogLevel } from '@reservoir0x/reservoir-sdk'
 
 //CONFIGURABLE: Use nextjs to load your own custom font: https://nextjs.org/docs/basic-features/font-optimization
 const inter = Inter({
@@ -160,7 +159,6 @@ function MyApp({
             }),
             source: source,
             normalizeRoyalties: NORMALIZE_ROYALTIES,
-            logLevel: LogLevel.Verbose,
           }}
           theme={reservoirKitTheme}
         >
