320ff25338dd1af8dd795f8841c207713588805d
TomCat <midoelmelegy@gmail.com>
2023-04-14 00:31:31 +0800

    Update SeaPortLogoWhiteText.tsx

--- public/SeaPortLogoWhiteText.tsx
+++ public/SeaPortLogoWhiteText.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ReservoirLogoWhiteText = (props: any) => {
+const SeaPortLogoWhiteText = (props: any) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -32,4 +32,4 @@ const ReservoirLogoWhiteText = (props: any) => {
   )
 }
 
-export default ReservoirLogoWhiteText
+export default SeaPortLogoWhiteText
