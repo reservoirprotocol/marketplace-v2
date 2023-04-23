ffb8bc7b28b5576b999f42b622b32bcd1be0d8f2
TomCat <midoelmelegy@gmail.com>
2023-04-23 23:56:28 +0800

    Update index.tsx

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -137,7 +137,15 @@ const Navbar = () => {
               key={router.asPath}
             />
           </Box>
-          <Flex align="center" css={{ gap: '$3', mr: '$3' }}>
+          <Flex align="center"
+            css={{
+              gap: '$3',
+              mr: '$3',
+              '&:hover': {
+                background: '$gray8',
+              },
+            }}
+          >
             <Link href="/collection-rankings">
               <Button color="gray3">
                 <FontAwesomeIcon icon={faRankingStar} />
