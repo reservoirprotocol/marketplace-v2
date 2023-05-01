23c6d4a11bdb775e103906987d68c78a3b90690e
armando <armandomvillanueva@icloud.com>
2023-05-01 13:57:29 -0700

    feat: Block iframe

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,21 @@ const nextConfig = {
   experimental: {
     transpilePackages: ['@reservoir0x/reservoir-kit-ui'],
   },
+  headers: [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value: "frame-ancestors 'none'",
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+      ],
+    },
+  ],
 }
 
 export default nextConfig
