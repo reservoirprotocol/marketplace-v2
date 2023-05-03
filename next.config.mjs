5a0b58494a8b7ca674e2c81cb65c039bd02e0cfc
TomCat <midoelmelegy@gmail.com>
2023-05-03 15:13:41 +0800

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,23 @@ const nextConfig = {
   experimental: {
     transpilePackages: ['@reservoir0x/reservoir-kit-ui'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none'",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
