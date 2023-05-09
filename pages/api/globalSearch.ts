157ab9d2184950e7404f8d8f4a925d4fbb5d3929
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-05-09 15:22:46 -0500

    Search by text

--- pages/api/globalSearch.ts
+++ pages/api/globalSearch.ts
@@ -33,13 +33,13 @@ export default async function handler(req: Request) {
   const promises: ReturnType<typeof fetcher>[] = []
 
   let queryParams: paths['/search/collections/v1']['get']['parameters']['query'] =
-    {
-      name: query as string,
-      limit: 6,
-    }
+  {
+    name: query as string,
+    limit: 6,
+  }
 
   supportedChains.forEach(async (chain) => {
-    const { reservoirBaseUrl, apiKey, collectionSetId, community } = chain
+    const { reservoirBaseUrl, apiKey, collectionSetId, community, } = chain
     const headers = {
       headers: {
         'x-api-key': apiKey || '',
@@ -140,43 +140,107 @@ export default async function handler(req: Request) {
         },
       ]
     }
-  } else {
-    // Get current usd prices for each chain
-    const usdCoinPrices = await fetch(`${HOST_URL}/api/usdCoinConversion`).then(
-      (res) => res.json()
-    )
+  }
+  // else {
+  //   // Get current usd prices for each chain
+  //   const usdCoinPrices = await fetch(`${HOST_URL}/api/usdCoinConversion`).then(
+  //     (res) => res.json()
+  //   )
 
-    const responses = await Promise.all(promises)
-    responses.forEach((response, index) => {
-      const chainSearchResults = response.data.collections.map(
-        (collection: SearchCollection) => ({
-          type: 'collection',
-          data: {
-            ...collection,
-            chainName: supportedChains[index].name.toLowerCase(),
-            chainId: supportedChains[index].id,
-            lightChainIcon: supportedChains[index].lightIconUrl,
-            darkChainIcon: supportedChains[index].darkIconUrl,
-            volumeCurrencySymbol: supportedChains[index].nativeCurrency.symbol,
-            volumeCurrencyDecimals:
-              supportedChains[index].nativeCurrency.decimals,
-            tokenCount: collection.tokenCount,
-            allTimeUsdVolume:
-              (collection.allTimeVolume &&
-                collection.allTimeVolume *
-                  usdCoinPrices?.prices?.[index]?.current_price) ||
-              0,
-          },
-        })
+  //   const responses = await Promise.all(promises)
+  //   responses.forEach((response, index) => {
+  //     const chainSearchResults = response.data.collections.map(
+  //       (collection: SearchCollection) => ({
+  //         type: 'collection',
+  //         data: {
+  //           ...collection,
+  //           chainName: supportedChains[index].name.toLowerCase(),
+  //           chainId: supportedChains[index].id,
+  //           lightChainIcon: supportedChains[index].lightIconUrl,
+  //           darkChainIcon: supportedChains[index].darkIconUrl,
+  //           volumeCurrencySymbol: supportedChains[index].nativeCurrency.symbol,
+  //           volumeCurrencyDecimals:
+  //             supportedChains[index].nativeCurrency.decimals,
+  //           tokenCount: collection.tokenCount,
+  //           allTimeUsdVolume:
+  //             (collection.allTimeVolume &&
+  //               collection.allTimeVolume *
+  //                 usdCoinPrices?.prices?.[index]?.current_price) ||
+  //             0,
+  //         },
+  //       })
+  //     )
+  //     searchResults = [...searchResults, ...chainSearchResults]
+  //   })
+
+  //   // Sort results by all time usd volume only if usdCoinPrices is not null
+  //   if (usdCoinPrices) {
+  //     searchResults = searchResults.sort(
+  //       (a, b) => b.data.allTimeUsdVolume - a.data.allTimeUsdVolume
+  //     )
+  //   }
+  // }
+  else {
+
+    const promises = supportedChains.map(async (chain) => {
+      const { reservoirBaseUrl, apiKey } = chain
+      const headers = {
+        headers: {
+          'x-api-key': apiKey || '',
+        },
+      }
+      const { data } = await fetcher(
+        `${reservoirBaseUrl}/collections/v2?name=${query}&limit=6`,
+        {},
+        headers
       )
-      searchResults = [...searchResults, ...chainSearchResults]
+      return data.collections.map((collection: Collection) => {
+        const processedCollection: SearchCollection = {
+          collectionId: collection.id,
+          contract: collection.primaryContract,
+          image: collection.image,
+          name: collection.name,
+          allTimeVolume: collection.volume?.allTime,
+          floorAskPrice: collection.floorAsk?.price?.amount?.decimal,
+          openseaVerificationStatus: collection.openseaVerificationStatus,
+          chainName: chain.name.toLowerCase(),
+          chainId: chain.id,
+          lightChainIcon: chain.lightIconUrl,
+          darkChainIcon: chain.darkIconUrl,
+          volumeCurrencySymbol: chain.nativeCurrency.symbol,
+          volumeCurrencyDecimals: chain.nativeCurrency.decimals,
+          tokenCount: collection.tokenCount || '0',
+        }
+        return {
+          type: 'collection',
+          data: processedCollection,
+        }
+      })
     })
 
-    // Sort results by all time usd volume only if usdCoinPrices is not null
-    if (usdCoinPrices) {
-      searchResults = searchResults.sort(
-        (a, b) => b.data.allTimeUsdVolume - a.data.allTimeUsdVolume
-      )
+    let results = await Promise.allSettled(promises).then((results) => {
+      return results
+        .filter(
+          (result) => result.status === 'fulfilled' && result.value.length > 0
+        )
+        .flatMap((result) => (result as PromiseFulfilledResult<any>).value)
+    })
+
+    if (results.length > 0) {
+      searchResults = results
+    } else {
+      let ensData = await fetch(
+        `https://api.ensideas.com/ens/resolve/${query}`
+      ).then((res) => res.json())
+      searchResults = [
+        {
+          type: 'wallet',
+          data: {
+            ...ensData,
+            address: query,
+          },
+        },
+      ]
     }
   }
 
