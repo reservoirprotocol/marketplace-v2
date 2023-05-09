9b1725abf4d3f64c467f9618e5aa62cf310aaea0
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-05-09 18:42:56 -0500

    Update HamburgerMenu.tsx
    Update Aura logo

--- components/navbar/HamburgerMenu.tsx
+++ components/navbar/HamburgerMenu.tsx
@@ -68,10 +68,10 @@ const HamburgerMenu = () => {
           <Link href="/">
             <Box css={{ width: 34, cursor: 'pointer' }}>
               <Image
-                src="/reservoirLogo.svg"
+                src="/aura.png"
                 width={34}
                 height={39}
-                alt="Reservoir"
+                alt="Aura"
               />
             </Box>
           </Link>
