c1eedb59bad82125c2ed34c85f5bb9e43d317257
Ted Palmer <etpalmer17@gmail.com>
2023-05-09 12:03:59 -0400

    Fix offers and listings tabs passing undefined token or collection

--- components/token/ListingsTable.tsx
+++ components/token/ListingsTable.tsx
@@ -116,6 +116,7 @@ export const ListingsTable: FC<Props> = ({
                 <ListingTableRow
                   key={`${listing?.id}-${i}`}
                   listing={listing}
+                  tokenString={token}
                   address={address}
                   is1155={is1155}
                   isOwner={isOwner}
@@ -139,6 +140,7 @@ export const ListingsTable: FC<Props> = ({
 
 type ListingTableRowProps = {
   listing: ReturnType<typeof useListings>['data'][0]
+  tokenString: Parameters<typeof useListings>['0']['token']
   is1155: boolean
   isOwner: boolean
   address?: string
@@ -147,6 +149,7 @@ type ListingTableRowProps = {
 
 const ListingTableRow: FC<ListingTableRowProps> = ({
   listing,
+  tokenString,
   is1155,
   isOwner,
   address,
@@ -158,6 +161,9 @@ const ListingTableRow: FC<ListingTableRowProps> = ({
   const expirationText = expiration ? `Expires ${expiration}` : null
 
   const isUserListing = address?.toLowerCase() === listing.maker.toLowerCase()
+
+  const contract = tokenString?.split(':')[0]
+  const tokenId = tokenString?.split(':')[1]
 
   const orderZone = listing?.rawData?.zone
   const orderKind = listing?.kind
@@ -248,8 +254,8 @@ const ListingTableRow: FC<ListingTableRowProps> = ({
           {/* Not owner, erc 721 */}
           {!isOwner && !is1155 ? (
             <BuyNow
-              tokenId={listing.criteria?.data?.token?.tokenId}
-              collectionId={listing.criteria?.data?.collection?.id}
+              tokenId={listing.criteria?.data?.token?.tokenId || tokenId}
+              collectionId={listing.criteria?.data?.collection?.id || contract}
               orderId={listing.id}
               buttonChildren="Buy"
               buttonCss={{ fontSize: 14, px: '$4', py: '$2', minHeight: 36 }}
@@ -276,8 +282,10 @@ const ListingTableRow: FC<ListingTableRowProps> = ({
               {isOracleOrder ? (
                 <EditListing
                   listingId={listing.id}
-                  tokenId={listing.criteria?.data?.token?.tokenId}
-                  collectionId={listing.criteria?.data?.collection?.id}
+                  tokenId={listing.criteria?.data?.token?.tokenId || tokenId}
+                  collectionId={
+                    listing.criteria?.data?.collection?.id || contract
+                  }
                   buttonChildren={<Text style="subtitle2">Edit</Text>}
                   buttonCss={{
                     fontSize: 14,
