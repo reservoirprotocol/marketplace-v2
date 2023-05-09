58bbeebfeb27615a5b578fb3133990c719ffc2c4
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-05-08 23:59:01 -0500

    Add files via upload

--- components/buttons/BuyNow.tsx
+++ components/buttons/BuyNow.tsx
@@ -1,22 +1,23 @@
-import React, { ComponentProps, FC, ReactNode } from 'react'
-import { SWRResponse } from 'swr'
-import { useNetwork, useSigner } from 'wagmi'
-import { BuyModal, BuyStep, useTokens } from '@reservoir0x/reservoir-kit-ui'
-import { useSwitchNetwork } from 'wagmi'
-import { Button } from 'components/primitives'
-import { useConnectModal } from '@rainbow-me/rainbowkit'
-import { CSS } from '@stitches/react'
-import { useMarketplaceChain } from 'hooks'
+import React, { ComponentProps, FC, ReactNode } from 'react';
+import { SWRResponse } from 'swr';
+import { useNetwork, useSigner } from 'wagmi';
+import { BuyModal, BuyStep, useTokens } from '@reservoir0x/reservoir-kit-ui';
+import { useSwitchNetwork } from 'wagmi';
+import { Button } from 'components/primitives';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
+import { CSS } from '@stitches/react';
+import { useMarketplaceChain } from 'hooks';
+import va from '@vercel/analytics';
 
 type Props = {
-  tokenId?: string
-  collectionId?: string
-  orderId?: string
-  buttonCss?: CSS
-  buttonProps?: ComponentProps<typeof Button>
-  buttonChildren?: ReactNode
-  mutate?: SWRResponse['mutate']
-}
+  tokenId?: string;
+  collectionId?: string;
+  orderId?: string;
+  buttonCss?: CSS;
+  buttonProps?: ComponentProps<typeof Button>;
+  buttonChildren?: ReactNode;
+  mutate?: SWRResponse['mutate'];
+};
 
 const BuyNow: FC<Props> = ({
   tokenId,
@@ -27,60 +28,66 @@ const BuyNow: FC<Props> = ({
   buttonProps = {},
   buttonChildren,
 }) => {
-  const { data: signer } = useSigner()
-  const { openConnectModal } = useConnectModal()
-  const { chain: activeChain } = useNetwork()
-  const marketplaceChain = useMarketplaceChain()
+  const { data: signer } = useSigner();
+  const { openConnectModal } = useConnectModal();
+  const { chain: activeChain } = useNetwork();
+  const marketplaceChain = useMarketplaceChain();
   const { switchNetworkAsync } = useSwitchNetwork({
     chainId: marketplaceChain.id,
-  })
+  });
   const isInTheWrongNetwork = Boolean(
     signer && activeChain?.id !== marketplaceChain.id
-  )
+  );
 
   const trigger = (
     <Button css={buttonCss} color="primary" {...buttonProps}>
       {buttonChildren}
     </Button>
-  )
-  const canBuy = signer && tokenId && collectionId && !isInTheWrongNetwork
+  );
+
+  const handleClick = async () => {
+    va.track('Buy Now');
+    if (isInTheWrongNetwork && switchNetworkAsync) {
+      const chain = await switchNetworkAsync(marketplaceChain.id);
+      if (chain.id !== marketplaceChain.id) {
+        return false;
+      }
+    }
+
+    if (!signer) {
+      openConnectModal?.();
+    }
+  };
+
+  const canBuy = signer && tokenId && collectionId && !isInTheWrongNetwork;
 
   return !canBuy ? (
     <Button
       css={buttonCss}
       aria-haspopup="dialog"
       color="primary"
-      onClick={async () => {
-        if (isInTheWrongNetwork && switchNetworkAsync) {
-          const chain = await switchNetworkAsync(marketplaceChain.id)
-          if (chain.id !== marketplaceChain.id) {
-            return false
-          }
-        }
-
-        if (!signer) {
-          openConnectModal?.()
-        }
-      }}
+      onClick={handleClick}
       {...buttonProps}
     >
       {buttonChildren}
     </Button>
   ) : (
     <BuyModal
-      trigger={trigger}
+      trigger={React.cloneElement(trigger, {
+        onClick: () => {
+          va.track('Buy Now');
+        },
+      })}
       tokenId={tokenId}
       collectionId={collectionId}
       orderId={orderId}
-      //CONFIGURABLE: set any fees on top of orders, note that these will only
-      // apply to native orders (using the reservoir order book) and not to external orders (opensea, blur etc)
-      // referrer={"0xabc"}
-      // referrerFeeBps={250}
+      referrer={'0xd2f6BF9e792DbBCA466D93bCC5a939e2D1c72B2B'}
+      referrerFeeBps={100}
       onClose={(data, stepData, currentStep) => {
-        if (mutate && currentStep == BuyStep.Complete) mutate()
+        if (mutate && currentStep == BuyStep.Complete) mutate();
       }}
     />
-  )
-}
+  );
+};
 
-export default BuyNow
+export default BuyNow;
--- components/buttons/List.tsx
+++ components/buttons/List.tsx
@@ -1,5 +1,5 @@
-import { ListModal, ListStep, useTokens } from '@reservoir0x/reservoir-kit-ui'
-import { Button } from 'components/primitives'
+import { ListModal, ListStep, useTokens } from '@reservoir0x/reservoir-kit-ui';
+import { Button } from 'components/primitives';
 import {
   cloneElement,
   ComponentProps,
@@ -7,25 +7,26 @@ import {
   FC,
   ReactNode,
   useContext,
-} from 'react'
-import { CSS } from '@stitches/react'
-import { SWRResponse } from 'swr'
-import { useAccount, useNetwork, useSigner, useSwitchNetwork } from 'wagmi'
-import { useConnectModal } from '@rainbow-me/rainbowkit'
-import { ToastContext } from 'context/ToastContextProvider'
-import { useMarketplaceChain } from 'hooks'
+} from 'react';
+import { CSS } from '@stitches/react';
+import { SWRResponse } from 'swr';
+import { useAccount, useNetwork, useSigner, useSwitchNetwork } from 'wagmi';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
+import { ToastContext } from 'context/ToastContextProvider';
+import { useMarketplaceChain } from 'hooks';
+import va from '@vercel/analytics';
 
 type ListingCurrencies = ComponentPropsWithoutRef<
   typeof ListModal
->['currencies']
+>['currencies'];
 
 type Props = {
-  token?: ReturnType<typeof useTokens>['data'][0]
-  buttonCss?: CSS
-  buttonChildren?: ReactNode
-  buttonProps?: ComponentProps<typeof Button>
-  mutate?: SWRResponse['mutate']
-}
+  token?: ReturnType<typeof useTokens>['data'][0];
+  buttonCss?: CSS;
+  buttonChildren?: ReactNode;
+  buttonProps?: ComponentProps<typeof Button>;
+  mutate?: SWRResponse['mutate'];
+};
 
 const List: FC<Props> = ({
   token,
@@ -34,73 +35,95 @@ const List: FC<Props> = ({
   buttonProps,
   mutate,
 }) => {
-  const { isDisconnected } = useAccount()
-  const { openConnectModal } = useConnectModal()
-  const { addToast } = useContext(ToastContext)
+  const { isDisconnected } = useAccount();
+  const { openConnectModal } = useConnectModal();
+  const { addToast } = useContext(ToastContext);
 
-  const marketplaceChain = useMarketplaceChain()
+  const marketplaceChain = useMarketplaceChain();
   const { switchNetworkAsync } = useSwitchNetwork({
     chainId: marketplaceChain.id,
-  })
+  });
 
-  const { data: signer } = useSigner()
-  const { chain: activeChain } = useNetwork()
+  const { data: signer } = useSigner();
+  const { chain: activeChain } = useNetwork();
 
   const isInTheWrongNetwork = Boolean(
     signer && marketplaceChain.id !== activeChain?.id
-  )
+  );
 
-  let listingCurrencies: ListingCurrencies = undefined
+  let listingCurrencies: ListingCurrencies = undefined;
 
-  const tokenId = token?.token?.tokenId
-  const contract = token?.token?.contract
+  const tokenId = token?.token?.tokenId;
+  const contract = token?.token?.contract;
 
   const trigger = (
     <Button css={buttonCss} color="primary" {...buttonProps}>
       {buttonChildren}
     </Button>
-  )
+  );
+
+  const handleClick = async () => {
+    va.track('List Token');
+    if (switchNetworkAsync && activeChain) {
+      const chain = await switchNetworkAsync(marketplaceChain.id);
+      if (chain.id !== marketplaceChain.id) {
+        return false;
+      }
+    }
+
+    if (!signer) {
+      openConnectModal?.();
+    }
+  };
 
   if (isDisconnected || isInTheWrongNetwork) {
     return cloneElement(trigger, {
-      onClick: async () => {
-        if (switchNetworkAsync && activeChain) {
-          const chain = await switchNetworkAsync(marketplaceChain.id)
-          if (chain.id !== marketplaceChain.id) {
-            return false
-          }
-        }
-
-        if (!signer) {
-          openConnectModal?.()
-        }
-      },
-    })
+      onClick: handleClick,
+    });
   } else
     return (
       <ListModal
-        trigger={trigger}
+        trigger={cloneElement(trigger, {
+          onClick: () => {
+            va.track('List Token');
+          },
+        })}
         collectionId={contract}
         tokenId={tokenId}
-        currencies={listingCurrencies}
+        currencies={[
+          {
+            contract: '0x0000000000000000000000000000000000000000',
+            symbol: 'ETH',
+          },
+          {
+            contract: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+            symbol: 'USDC',
+            decimals: 6
+          },
+          {
+            contract: '0x6982508145454ce325ddbe47a25d4ec3d2311933',
+            symbol: 'PEPE',
+            decimals: 18
+          },
+        ]}
         onClose={(data, stepData, currentStep) => {
-          if (mutate && currentStep == ListStep.Complete) mutate()
+          if (mutate && currentStep == ListStep.Complete) mutate();
         }}
         onListingError={(err: any) => {
           if (err?.code === 4001) {
             addToast?.({
               title: 'User canceled transaction',
               description: 'You have canceled the transaction.',
-            })
-            return
+            });
+            return;
           }
           addToast?.({
             title: 'Could not list token',
             description: 'The transaction was not completed.',
-          })
+          });
         }}
       />
-    )
-}
+    );
+};
 
-export default List
+export default List;
