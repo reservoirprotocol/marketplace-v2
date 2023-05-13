e9a97101ace2a0077266754ded72951a7fd82e7a
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-05-12 23:46:20 -0500

    Update domains.tsx

--- pages/domains.tsx
+++ pages/domains.tsx
@@ -10,7 +10,7 @@ const AuraDomains: NextPage = () => {
             <Flex
                 direction="column"
                 align="center"
-                css={{ py: '200px', px: '$3', textAlign: 'center' }}
+                css={{ py: '50px', px: '$3', textAlign: 'center' }}
             >
                 {/*
                 <Text style="h1" as="h1">
