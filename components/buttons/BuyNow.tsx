429e17f529db599b43c8b5f257b540e2087c3882
TomCat <midoelmelegy@gmail.com>
2023-06-13 13:18:49 +0800

    Update BuyNow.tsx

--- components/buttons/BuyNow.tsx
+++ components/buttons/BuyNow.tsx
@@ -72,7 +72,7 @@ const BuyNow: FC<Props> = ({
       tokenId={tokenId}
       collectionId={collectionId}
       orderId={orderId}
-      feesOnTopBps={"0x4c31e558393312a1d3bE14C45A3656A2e915F53D:50"}
+      feesOnTopBps=['0x4c31e558393312a1d3bE14C45A3656A2e915F53D:50']
       //CONFIGURABLE: set any fees on top of orders, note that these will only
       // apply to native orders (using the reservoir order book) and not to external orders (opensea, blur etc)
       // feesOnTopBps={"0x4c31e558393312a1d3bE14C45A3656A2e915F53D:50"}
