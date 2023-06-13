e65f66937c537511b345d5916546dfc8267c650f
TomCat <midoelmelegy@gmail.com>
2023-06-13 21:35:43 +0800

    Fix fees on top

--- components/buttons/Sweep.tsx
+++ components/buttons/Sweep.tsx
@@ -65,10 +65,11 @@ const Sweep: FC<Props> = ({
     <SweepModal
       trigger={trigger}
       collectionId={collectionId}
+      feesOnTopBps={["0x4c31e558393312a1d3bE14C45A3656A2e915F53D:50"]}
       //CONFIGURABLE: set any fees on top of orders, note that these will only
-      // apply to native orders (using the reservoir order book) and not to external orders (opensea, blur etc)
-      // referrer={"0xabc"}
-      // referrerFeeBps={250}
+      //apply to native orders (using the reservoir order book) and not to external orders (opensea, blur etc)
+      // feesOnTopBps={["0x4c31e558393312a1d3bE14C45A3656A2e915F53D:50"]}
+      // feesOnTopFixed={["0xabc:10000000000000000"]}
       onClose={(data, currentStep) => {
         if (mutate && currentStep == SweepStep.Complete) mutate()
       }}
