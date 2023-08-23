1d3341008feb1451dbcb7a7974ca6dc02ea3ef04
Michael Morrissey <mikehmorrissey@gmail.com>
2023-08-23 15:38:27 -0400

    use dynamic site url to fetch trending collections

--- pages/[chain]/index.tsx
+++ pages/[chain]/index.tsx
@@ -71,7 +71,7 @@ const Home: NextPage<any> = ({ ssr }) => {
         .filter((c) => c.id !== chain.id)
         .forEach((c) => {
           preload(
-            `http://localhost:3000/api/${c.routePrefix}/trendingCollections/v1`,
+            `${process.env.NEXT_PUBLIC_HOST_URL}/api/${c.routePrefix}/trendingCollections/v1`,
             fetcher
           )
         }),
@@ -537,7 +537,7 @@ export const getServerSideProps: GetServerSideProps<{
     DefaultChain
 
   const response = await fetcher(
-    `http://localhost:3000/api/${chainPrefix}/trendingCollections/v1`,
+    `${process.env.NEXT_PUBLIC_HOST_URL}/api/${chainPrefix}/trendingCollections/v1`,
     {
       headers: {
         'x-api-key': chain.apiKey || '',
