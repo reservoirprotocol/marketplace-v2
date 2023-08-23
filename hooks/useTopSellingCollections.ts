e54edb9366f4a397642842bb0d2926e5970b65b7
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-08-23 15:13:45 -0400

    fix type conflict

--- hooks/useTopSellingCollections.ts
+++ hooks/useTopSellingCollections.ts
@@ -62,6 +62,7 @@ export default function (
       return {
         ...collection,
         ...collectionsMap[collection.id as string],
+        volume: collection.volume,
       }
     })
   }, [collections, topSellingData])
