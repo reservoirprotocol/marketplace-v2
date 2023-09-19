c413e2f4351ba3bea0605c60f6b94c79dea08cb3
armando <armandomvillanueva@icloud.com>
2023-09-18 22:34:19 -0700

    feat: Change icon & copy

--- pages/500.tsx
+++ pages/500.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next'
 import { Text, Flex, Box } from 'components/primitives'
 import Layout from 'components/Layout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFolderOpen } from '@fortawesome/free-solid-svg-icons'
+import { faWarning } from '@fortawesome/free-solid-svg-icons'
 
 const IndexPage: NextPage = () => {
   return (
@@ -13,14 +13,13 @@ const IndexPage: NextPage = () => {
         css={{ py: '200px', px: '$3', textAlign: 'center' }}
       >
         <Box css={{ color: '$gray11', mb: '30px' }}>
-          <FontAwesomeIcon icon={faFolderOpen} size="2xl" />
+          <FontAwesomeIcon icon={faWarning} size="2xl" />
         </Box>
         <Text style="body1" color="subtle" css={{ mb: '$1' }}>
           500 Error.
         </Text>
         <Text style="body1" color="subtle">
-          Oops! Something went wrong. We're on it. Try refreshing or check back
-          soon.
+          Something went wrong. Try refreshing or check back soon.
         </Text>
       </Flex>
     </Layout>
