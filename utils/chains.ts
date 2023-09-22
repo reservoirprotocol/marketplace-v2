04931ee02186ec7e946621f7210577ecbf6a238e
armando <armandomvillanueva@icloud.com>
2023-09-22 11:56:01 -0700

    feat: Add sepolia dev

--- utils/chains.ts
+++ utils/chains.ts
@@ -1,6 +1,6 @@
 import { Currency } from '@reservoir0x/reservoir-kit-ui'
 import { zeroAddress } from 'viem'
-import { Chain, goerli } from 'wagmi/chains'
+import { Chain, goerli, sepolia } from 'wagmi/chains'
 import usdcContracts from './usdcContracts'
 import wrappedContracts from './wrappedContracts'
 
@@ -79,4 +79,33 @@ export const DefaultChain: ReservoirChain = {
   oracleBidsEnabled: true,
 }
 
-export default [DefaultChain] as ReservoirChain[]
+export default [
+  {
+    ...sepolia,
+    // Any url to display the logo of the chain in light mode
+    lightIconUrl: '/icons/goerli-icon-dark.svg',
+    // Any url to display the logo of the chain in dark mode
+    darkIconUrl: '/icons/goerli-icon-light.svg',
+    // The base url of the reservoir api, this is used in the app when
+    // directly interacting with the reservoir indexer servers (in the api proxy for example)
+    // or when prefetching server side rendered data
+    reservoirBaseUrl: 'https://api-sepolia-dev.reservoir.tools',
+    // Used on the client side portions of the marketplace that need an api key added
+    // Prevents the api key from being leaked in the clientside requests
+    // If you'd like to disable proxying you can just change the proxyApi to the reservoirBaseUrl
+    // Doing so will omit the api key unless further changes are made
+    proxyApi: '',
+    // A prefix used in the asset specific routes on the app (tokens/collections)
+    routePrefix: 'sepolia',
+    // Reservoir API key which you can generate at https://reservoir.tools/
+    // This is a protected key and displays as 'undefined' on the browser
+    // DO NOT add NEXT_PUBLIC to the key or you'll risk leaking it on the browser
+    apiKey: process.env.RESERVOIR_API_KEY,
+    // Coingecko id, used to convert the chain's native prices to usd. Can be found here:
+    // https://www.coingecko.com/en/api/documentation#operations-coins-get_coins_list
+    coingeckoId: 'ethereum',
+    collectionSetId: process.env.NEXT_PUBLIC_SEPOLIA_COLLECTION_SET_ID,
+    community: process.env.NEXT_PUBLIC_SEPOLIA_COMMUNITY,
+  },
+  DefaultChain,
+] as ReservoirChain[]
