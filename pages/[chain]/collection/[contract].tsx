ee8c72edff341f4e668d52ac2d7b4efbfb7cd125
armando <armandomvillanueva@icloud.com>
2023-11-09 13:35:07 -0800

    feat: Fix mobile styling

--- pages/[chain]/collection/[contract].tsx
+++ pages/[chain]/collection/[contract].tsx
@@ -136,8 +136,8 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
 
   const mintPrice =
     typeof mintPriceDecimal === 'number' &&
-    mintPriceDecimal !== null &&
-    mintPriceDecimal !== undefined
+      mintPriceDecimal !== null &&
+      mintPriceDecimal !== undefined
       ? mintPriceDecimal === 0
         ? 'Free'
         : `${mintPriceDecimal} ${mintCurrency}`
@@ -233,8 +233,8 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
               sortBy === 'rarity'
                 ? tokenIndex
                 : endOfListingsIndex > -1
-                ? endOfListingsIndex
-                : 0
+                  ? endOfListingsIndex
+                  : 0
             newTokens.splice(newTokenIndex, 0, {
               ...token,
               market: {
@@ -266,17 +266,17 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
             sortBy === 'rarity'
               ? tokenIndex
               : tokens.findIndex((token) => {
-                  let currentTokenPrice =
-                    token.market?.floorAsk?.price?.amount?.native
-                  if (currentTokenPrice !== undefined) {
-                    return sortDirection === 'desc'
-                      ? updatedToken.market.floorAsk.price.amount.native >=
-                          currentTokenPrice
-                      : updatedToken.market.floorAsk.price.amount.native <=
-                          currentTokenPrice
-                  }
-                  return true
-                })
+                let currentTokenPrice =
+                  token.market?.floorAsk?.price?.amount?.native
+                if (currentTokenPrice !== undefined) {
+                  return sortDirection === 'desc'
+                    ? updatedToken.market.floorAsk.price.amount.native >=
+                    currentTokenPrice
+                    : updatedToken.market.floorAsk.price.amount.native <=
+                    currentTokenPrice
+                }
+                return true
+              })
           if (updatedTokenPosition <= -1) {
             return
           }
@@ -324,9 +324,9 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
 
   const rarityEnabledCollection = Boolean(
     collection?.tokenCount &&
-      +collection.tokenCount >= 2 &&
-      attributes &&
-      attributes?.length >= 2
+    +collection.tokenCount >= 2 &&
+    attributes &&
+    attributes?.length >= 2
   )
 
   const hasSecurityConfig =
@@ -334,7 +334,7 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
     Object.values(collection.securityConfig).some(Boolean)
   const contractKind = `${collection?.contractKind?.toUpperCase()}${
     hasSecurityConfig ? 'C' : ''
-  }`
+    }`
 
   useEffect(() => {
     const isVisible = !!loadMoreObserver?.isIntersecting
@@ -426,7 +426,10 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
                         }
                       />
                     </Flex>
-                    <Flex css={{ gap: '$3' }} align="center">
+                    <Flex css={{
+                      gap: '$3',
+                      ...(isSmallDevice && { display: 'grid', gridTemplateColumns: '1fr 1fr' }),
+                    }} align="center">
                       <CopyText
                         text={collection.id as string}
                         css={{
@@ -543,16 +546,16 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
                       openState={isMintRoute ? mintOpenState : undefined}
                       buttonChildren={
                         <Flex
-                          css={{ gap: '$2', px: '$4' }}
+                          css={{ gap: '$2', px: '$2' }}
                           align="center"
                           justify="center"
                         >
                           {isSmallDevice && (
                             <FontAwesomeIcon icon={faSeedling} />
                           )}
-                          <Text style="h6" as="h6" css={{ color: '$bg' }}>
+                          {!isSmallDevice && <Text style="h6" as="h6" css={{ color: '$bg' }}>
                             Mint
-                          </Text>
+                          </Text>}
 
                           {!isSmallDevice && (
                             <Text
@@ -575,7 +578,6 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
                         maxWidth: '220px',
                         '@md': {
                           order: 1,
-                          px: '$5',
                         },
                       }}
                       mutate={mutate}
@@ -726,9 +728,8 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
                       </Text>
                       <Text style="body1" as="p" css={{ fontWeight: '700' }}>
                         {topBidPrice
-                          ? `${topBidPrice?.toFixed(2) || 0} ${
-                              chainCurrency.symbol
-                            }`
+                          ? `${topBidPrice?.toFixed(2) || 0} ${chainCurrency.symbol
+                          }`
                           : '-'}
                       </Text>
                     </Flex>
@@ -755,37 +756,37 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
                   >
                     {isFetchingInitialData
                       ? Array(10)
-                          .fill(null)
-                          .map((_, index) => (
-                            <LoadingCard key={`loading-card-${index}`} />
-                          ))
+                        .fill(null)
+                        .map((_, index) => (
+                          <LoadingCard key={`loading-card-${index}`} />
+                        ))
                       : tokens.map((token, i) => (
-                          <TokenCard
-                            key={i}
-                            token={token}
-                            address={address as Address}
-                            mutate={mutate}
-                            rarityEnabled={rarityEnabledCollection}
-                            onMediaPlayed={(e) => {
-                              if (
-                                playingElement &&
-                                playingElement !== e.nativeEvent.target
-                              ) {
-                                playingElement.pause()
-                              }
-                              const element =
-                                (e.nativeEvent.target as HTMLAudioElement) ||
-                                (e.nativeEvent.target as HTMLVideoElement)
-                              if (element) {
-                                setPlayingElement(element)
-                              }
-                            }}
-                            addToCartEnabled={
-                              token.market?.floorAsk?.maker?.toLowerCase() !==
-                              address?.toLowerCase()
+                        <TokenCard
+                          key={i}
+                          token={token}
+                          address={address as Address}
+                          mutate={mutate}
+                          rarityEnabled={rarityEnabledCollection}
+                          onMediaPlayed={(e) => {
+                            if (
+                              playingElement &&
+                              playingElement !== e.nativeEvent.target
+                            ) {
+                              playingElement.pause()
                             }
-                          />
-                        ))}
+                            const element =
+                              (e.nativeEvent.target as HTMLAudioElement) ||
+                              (e.nativeEvent.target as HTMLVideoElement)
+                            if (element) {
+                              setPlayingElement(element)
+                            }
+                          }}
+                          addToCartEnabled={
+                            token.market?.floorAsk?.maker?.toLowerCase() !==
+                            address?.toLowerCase()
+                          }
+                        />
+                      ))}
                     <Box
                       ref={loadMoreRef}
                       css={{
@@ -896,11 +897,11 @@ export const getServerSideProps: GetServerSideProps<{
   }
 
   let collectionQuery: paths['/collections/v7']['get']['parameters']['query'] =
-    {
-      id,
-      includeSalesCount: true,
-      normalizeRoyalties: NORMALIZE_ROYALTIES,
-    }
+  {
+    id,
+    includeSalesCount: true,
+    normalizeRoyalties: NORMALIZE_ROYALTIES,
+  }
 
   const collectionsPromise = fetcher(
     `${reservoirBaseUrl}/collections/v7`,
