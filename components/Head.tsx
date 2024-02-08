f8895fe13446d2565ed82ed5dc1a34e3b6a3e767
armando <armandomvillanueva@icloud.com>
2024-02-07 17:37:36 -0800

    feat: force tag spot

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react'
 import NextHead from 'next/head'
+import { Token } from '@reservoir0x/reservoir-kit-ui'
 
 type Props = {
   ogImage?: string
   title?: string
   description?: string
-  metatags?: React.ReactNode
+  token?: Token
 }
 
 /**
@@ -18,7 +19,7 @@ export const Head: FC<Props> = ({
   ogImage = 'https://explorer.reservoir.tools/og-image.png',
   title = 'Reservoir | Multi-Chain NF`T Explorer',
   description = 'Reservoir Multi-Chain NFT Explorer is an open source NFT explorer built with Reservoir.',
-  metatags = null,
+  token,
 }) => {
   return (
     <NextHead>
@@ -38,20 +39,39 @@ export const Head: FC<Props> = ({
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImage} />
 
-      {/* Open Graph */}
-      <meta property="og:type" content="website" />
-      <meta property="og:determiner" content="the" />
-      <meta property="og:locale" content="en" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={ogImage} />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta property="og:image:alt" content="Reservoir NFT Explorer Banner" />
-
       {/* Extra Metatas */}
-      {metatags && metatags}
+      <>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content={token?.token?.imageSmall} />
+        <meta property="fc:frame:button:1" content="Mint" />
+        <meta property="fc:frame:button:1:action" content="mint" />
+        <meta
+          property="fc:frame:button:1:target"
+          content="eip155:7777777:0x2e92b5094c531257a9f427def7c05b412b9056bb:18"
+        />
+        <meta property="eth:nft:collection" content="taboo9" />
+        <meta property="eth:nft:status" content="live" />
+        <meta property="eth:nft:mint_count" content="1" />
+        <meta
+          property="eth:nft:creator_address"
+          content="0xf90665f14eefda57144441743816f4740cc89722"
+        />
+        <meta
+          property="eth:nft:contract_address"
+          content="0x2e92b5094c531257a9f427def7c05b412b9056bb"
+        />
+        <meta property="eth:nft:schema" content="ERC1155" />
+        <meta property="eth:nft:endtime" content="1709944871000" />
+        <meta property="og:image" content={token?.token?.imageSmall} />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:button:1" content="View on Reservoir" />
+        <meta property="fc:frame:button:1:action" content="post_redirect" />
+        <meta
+          property="fc:frame:post_url"
+          content="https://explorer.reservoir.tools/"
+        />
+        <meta property="fc:frame:image" content={token?.token?.imageSmall} />
+      </>
     </NextHead>
   )
 }
