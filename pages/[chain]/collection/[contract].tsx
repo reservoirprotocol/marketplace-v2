e93746fbbe0474ce176736d1a90d404af54acd86
armando <armandomvillanueva@icloud.com>
2024-02-22 12:07:11 -0800

    feat: remove uneeded tag

--- pages/[chain]/collection/[contract].tsx
+++ pages/[chain]/collection/[contract].tsx
@@ -388,10 +388,6 @@ const CollectionPage: NextPage<Props> = ({ id, ssr }) => {
               property="eth:nft:media_url"
               content={collection.banner || collection.image}
             />
-            <meta
-              property="eth:nft:chain"
-              content={client?.currentChain()?.name.toLowerCase()}
-            />
             <meta property="fc:frame:button:1" content="Mint" />
             <meta property="fc:frame:button:1:action" content="mint" />
             <meta
