ff63b3348a709f453afe3b45b2271466d9a41ca7
armando <armandomvillanueva@icloud.com>
2024-02-22 11:37:04 -0800

    feat: remove frame tags

--- pages/[chain]/asset/[assetId].tsx
+++ pages/[chain]/asset/[assetId].tsx
@@ -260,20 +260,6 @@ const IndexPage: NextPage<Props> = ({ assetId, ssr }) => {
               content={token?.token?.kind?.toUpperCase()}
             />
             <meta property="eth:nft:media_url" content={token?.token?.image} />
-
-            <meta property="fc:frame" content="vNext" />
-            <meta
-              property="fc:frame:image"
-              content={token?.token?.image || collection?.banner}
-            />
-            <meta property="fc:frame:button:1" content="Mint" />
-            <meta property="fc:frame:button:1:action" content="mint" />
-            <meta
-              property="fc:frame:button:1:target"
-              content={`${token?.token?.kind?.toUpperCase()}:${
-                token?.token?.chainId
-              }:${token?.token?.contract}:${token?.token?.tokenId}`}
-            />
           </>
         }
       />
