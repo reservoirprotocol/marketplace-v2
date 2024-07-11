e1987b5a8bab92b9ddc9e450d3643fd86676ac19
Jaden <jadendurnford@gmail.com>
2024-07-11 18:42:03 -0400

    feat: fixing nebula currencies

--- hooks/useChainCurrency.ts
+++ hooks/useChainCurrency.ts
@@ -1,4 +1,4 @@
-import { mainnet, sepolia } from 'wagmi/chains'
+import { mainnet, sepolia, skaleNebula } from 'wagmi/chains'
 import { useMarketplaceChain } from 'hooks'
 import { zeroAddress } from 'viem'
 
@@ -13,6 +13,14 @@ export default function () {
       decimals: 18,
       address: zeroAddress,
       chainId: chain?.id || mainnet.id,
+    }
+  } else if (chain.id === skaleNebula.id) {
+    return {
+      name: 'Europa ETH',
+      symbol: 'ETH',
+      decimals: 18,
+      address: '0xab01bad2c86e24d371a13ed6367bdca819589c5d',
+      chainId: 1482601649,
     }
   } else {
     return {
